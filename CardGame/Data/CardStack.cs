650e68951bbe2a3e23577d0ec075bb451c044e70
krumeluren <9928liqv@gmail.com>
2022-09-23 18:59:19 +0200

    Add minigame page
    Add all required blazor minigame page/components/classes

---
+++ CardGame/Data/CardStack.cs
@@ -0,0 +1,39 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Components;
+using ServiceContracts;
+
+namespace CardGame.Data;
+
+public class CardStack
+{
+    private IServiceManager _services { get; set; } = null!;
+
+    public List<Card> Cards { get; set; } = new List<Card>();
+
+    public CardStack(IServiceManager services)
+    {
+        _services = services;
+    }
+
+    public void GenerateStack()
+    {
+        Cards = _services.CardService.GetAll().ToList();
+    }
+
+    public List<Card> TakeRandom(int count = 1)
+    {
+        var cardsToTake = new List<Card>();
+        if(Cards.Count < count)
+        GenerateStack();
+        
+        for (int i = 0; i < count; i++)
+        {
+            var index = new Random().Next(Cards.Count());
+            var card = Cards[index];
+            cardsToTake.Add(card);
+            Cards.Remove(card);
+        }
+        return cardsToTake;
+    }
+
+}
---
+++ CardGame/Data/MinigameCardHolder.cs
@@ -0,0 +1,17 @@
+﻿using Domain;
+
+namespace CardGame.Data;
+
+/// <summary>
+/// Adapter for Card to be used in minigame with select events.
+/// </summary>
+public class MinigameCardHolder
+{
+    public Card Card { get; set; } = null!;
+    public bool Selected { get; set; }
+
+    public MinigameCardHolder(Card card)
+    {
+        Card = card;
+    }
+}
